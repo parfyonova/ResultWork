4ee9f9605ea3738f404faf2c7bc5306be2d995ce
Nadya <19nadya9425@gmail.com>
2022-11-29 23:49:21 +0300

    Добавление метода подсчета кол-ва элементов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите текст через запятую для проверки по длине элемента: ");
+string[] text = Console.ReadLine()!.Split(',');
+
+
+
+//Метод подсчета кол-ва элементов (меньше или равны 3)
+int Quantity(string[] massive, int num)
+{
+    for(int i = 0; i < massive.Length; i++)
+    {
+        if(massive[i].Length <= 3)
+        {
+            num++;
+        }
+    }
+    return num;
+}
